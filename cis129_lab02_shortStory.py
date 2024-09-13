4c4feb115c0b0694741f4f5b8fe8f987ef670bc4
ckiernan96 <ckiernan@mail.pima.edu>
2024-09-13 16:32:41 -0700

    Update cis129_lab02_shortStory.py
    Changes made for Step 5 of Module 2: Lab for CIS129 Course

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Christopher Kiernan 
+# My first Python lab for my CIS129 Course
+# Prints a short and simple story
+print(I want to learn more about coding.)
+print(Accuracy and knowledge will help me succeed.)
+print(Paying attention and studying are keys to success.)
+print(Hope I do well this semester!)
