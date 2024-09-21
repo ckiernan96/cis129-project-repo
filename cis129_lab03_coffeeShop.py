ac02f52cfdaef25f78db45edf182251f7937e12b
ckiernan96 <ckiernan@mail.pima.edu>
2024-09-20 19:04:58 -0700

    Update cis129_lab03_coffeeShop.py
    Updated to add Teas and Sandwiches to menu. Added more hospitality.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,29 +1,52 @@
 #cis129_lab03_coffeeShop.py
-print('***************************************\
-     \nMy Coffee and Muffin Shop')
-
+print('* * * * * * * * * * * * * * * * * * * * * * *\
+     \n     -Rainforest Breakfast Shop-')
+print()
 #Coffee Amount = value1
 value1 = int(input('Number of coffees bought?'))
-
+print()
 #Muffin Amount = value2
 value2 = int(input('Number of muffins bought?'))
-
-print('***************************************')
-print('***************************************\
-     \nMy Coffee and Muffin Shop Receipt')
-
-#total muffin cost = value3
-value3 = float(value1 * 5.00)
-print(value1, 'Coffee at $5 each:', '$', (format(value3, '.2f'))) 
-
-#total muffin cost = value4
-value4 = float(value2 * 4.00)
-print(value2, 'Muffins at $4 each:', '$',(format(value4, '.2f')))
-
-#tax total = value5
-value5 = (value3 + value4) * 0.06
-print('6% tax:', value5)
-
+print()
+#Tea Amount = value3
+value3 = int(input('Number of teas bought?'))
+print()
+#Sandwich Amount = value4
+value4 = int(input('Number of sandwiches bought?'))
+print()
+print('* ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *')
+print('* ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *')
+print()
+print('    Rainforest Breakfast Shop Receipt:')
+print()
+#total muffin cost = value5
+value5 = float(value1 * 5.00)
+print(value1, 'Coffee at $5 each:', '$', (format(value5, '.2f'))) 
+print()
+#total muffin cost = value6
+value6 = float(value2 * 4.00)
+print(value2, 'Muffins at $4 each:', '$',(format(value6, '.2f')))
+print()
+#total tea cost = value7
+value7 = float(value3 * 3.00)
+print(value3, 'Teas at $3 each:', '$',(format(value7, '.2f')))
+print()
+#total sandwich cost = value8
+value8 = float(value4 * 6.00)
+print(value4, 'Sandwiches at $6 each:', '$',(format(value8, '.2f')))
+print()
+print()
+#tax total = value9
+value9 = (value5 + value6 + value7 + value8) * 0.06
+print('6% tax:', (format(value9, '.2f')))
+print()
 print('---------')
-print('Total:', (value3 + value4 + value5))
-print('***************************************')
+print('Total:', format(value5 + value6 + value7 + value8 + value9, '.2f'))
+print()
+print('* * * * * * * * * * * * * * * * * * * * * * *')
+print()
+print('        Have a wonderful day!')
+print()
+print()
+print('Please leave us a good review on Yelp!\
+     \nIt really helps out the business')
